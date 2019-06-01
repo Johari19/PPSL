51adc7a4cefa49277399d63574d50be495f493ae
Joe <Johari19@users.noreply.github.com>
2019-05-31 23:00:23 -0400

    Version 0.0.2-a
    added example.py

---
+++ example.py
@@ -0,0 +1,7 @@
+from ppsl import *
+# import file like this ^
+pause()
+print("Hello!")
+choice()
+print("You chose to continue!")
+pause()
--- ppsl.py
+++ ppsl.py
@@ -2,21 +2,30 @@
 # Please visit my website for more info!
 # www.josephambayec.dev
 # Also see license for public use!
+# Setup
 import sys
 import time
-# Pause function
-def pause():
-    programPause = input("Press any key to continue...")
-# This will pause your program until user presses a key
-def choice():
-    programChoice = input("Would you like this process? Y/N: \n")
-    if (programChoice == "Y" or programChoice == "y"):
-        # Nothing
-        print("Continuing process!")
-        time.sleep(3)
-    else:
-        print("Stopping process")
-        time.sleep(3)
-        sys.exit()
-pause()
-choice()
+
+if __name__ == '__main__':
+    print("Do not run this program! Visit my site: www.josephambayec.dev")
+    time.sleep(10)
+else:
+    # Pause function
+    def pause():
+        # This will pause your program until user presses a key
+        programPause = input("Press any key to continue...")
+
+    # Choice function
+    def choice():
+        # This will give user choice to continue process or stop it
+        programChoice = input("Would you like this process? Y/N: \n")
+        if (programChoice == "Y" or programChoice == "y"):
+            # Nothing
+            print("Continuing process!")
+            time.sleep(3)
+        else:
+            print("Stopping process")
+            time.sleep(3)
+            sys.exit()
+
+    
